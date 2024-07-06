2d1ebf03dfb115f5e2a1aeed36f7b49a37dfea48
MatthewSeymour1 <N00233944@iadt.ie>
2024-07-06 22:27:29 +0100

    Added a square! It falls into the void though...

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if (e.KeyCode==Keys.A) 
+        // {
+        //     MessageBox.Show("A pressed");
+        // }
+    }
+}
